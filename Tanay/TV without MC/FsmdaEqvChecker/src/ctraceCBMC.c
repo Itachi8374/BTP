7b6b7e2b940ea49195eec87f565118254b181e2f
Tanay Maheshwari <tanaymodani18@gmail.com>
2023-03-13 11:25:48 +0530

    printing copies of input variables in cbmc

--- Tanay/TV without MC/FsmdaEqvChecker/src/ctraceCBMC.c
+++ Tanay/TV without MC/FsmdaEqvChecker/src/ctraceCBMC.c
@@ -14,6 +14,9 @@ int *inputVarCheck;
 
 
 PATH_PAIR_S * findcTrace(FSMD *, FSMD *,PATHS_LIST *,PATHS_LIST *,PATH_PAIR_S *,PATH_PAIR_S *,PATH_PAIR_S *);
+void inputVariablefind(FSMD *,int *);
+void printVarList();
+
 void printcTrace(FSMD*, PATHS_LIST*, PATH_PAIR_S*, int);
 void  cTraceUsingCBMC(FSMD *M0, FSMD *M1,PATHS_LIST *P0,PATHS_LIST *P1,PATH_PAIR_S *tempLIST,PATH_PAIR_S *tempE_u,PATH_PAIR_S *tempE_c, var_list *V0,var_list *V1)
 {
@@ -24,7 +27,16 @@ void  cTraceUsingCBMC(FSMD *M0, FSMD *M1,PATHS_LIST *P0,PATHS_LIST *P1,PATH_PAIR
 	finalTempLIST=findcTrace(M0,M1,P0,P1,tempLIST,tempE_u,tempE_c);
 	printcTrace(M0,P0,finalTempLIST,0);
 	printcTrace(M1,P1,finalTempLIST,1);
+  inputVariablefind(M0, inputVarCheck);
+  freopen("inputCBMC.c", "w", stdout);
+  printf("#include <assert.h>\n");
+  printf("void main(){\n");
+  printVarList();
+  printf("}");
+  fclose(stdout);
+  stdout = fdopen(0,"w");
 }
+
 
 
 PATH_PAIR_S* findcTrace(FSMD *M0, FSMD *M1,PATHS_LIST *P0,PATHS_LIST *P1,PATH_PAIR_S *tempLIST,PATH_PAIR_S *tempE_u,PATH_PAIR_S *tempE_c)
@@ -126,3 +138,84 @@ while(tempLIST!=(PATH_PAIR_S *)NULL)
 }
 }
 
+void inputVariablefind(FSMD *M,int *inputVarCheck)
+{
+	int i,j,k,flag;
+	TRANSITION_ST *temp;
+
+	printf("All Variables\n");
+	
+	for( j = 1; j < stab.numsymbols; j++ )
+	{
+		printf("%s\n",stab.sym_tab[j]);
+		inputVarCheck[j]=TRUE;
+	}
+	
+	
+	for(i=0;i<M->numstates;i++) 
+	{
+	 	temp=M->states[i].translist;
+	   	while(temp!=NULL) 
+		{
+	          //this loop will display all the transitions from a particular state
+              		j=0;
+			while( temp->action[j].rhs != NULL ) 
+			{
+				
+				for( k = 1; k < stab.numsymbols; k++ )
+				{
+					if(temp->action[j].lhs==stab.val_tab[k])
+					{
+					
+						inputVarCheck[k]=FALSE;
+						break;
+					}
+				}
+				j++;
+			}
+
+				temp=temp->next;
+		}
+
+	}
+	
+	printf("Input Variables\n");
+	for( j = 1; j < stab.numsymbols; j++ )
+	{
+		flag=0;
+		if(inputVarCheck[j]==TRUE)
+		{
+			for( i = 0; i < V0_V1.no_of_elements; i++ )
+			{
+				if(stab.val_tab[j]==V0_V1.var_val[i])
+				{
+					flag=1;
+					break;
+				}
+			}
+			if (flag==0)
+			inputVarCheck[j]=FALSE;
+		}
+	if(inputVarCheck[j]==TRUE)
+	printf("%s---%d--%d\n",stab.sym_tab[j],stab.val_tab[j],inputVarCheck[j]);
+		
+	}	
+}
+
+void printVarList()
+{
+	int j;
+	for( j = 1; j < stab.numsymbols; j++ )
+	{
+		if(inputVarCheck[j]==FALSE)
+		{
+			printf( "int %s%c%c%d;\n", stab.sym_tab[j], '_', 's', 0 );
+      printf( "int %s%c%c%d;\n", stab.sym_tab[j], '_', 's', 1 );
+      printf( "int %s%c%c%d;\n", stab.sym_tab[j], '_', 't', 0 );
+      printf( "int %s%c%c%d;\n", stab.sym_tab[j], '_', 't', 1 );
+		}
+		else
+		printf( "int %s;\n",stab.sym_tab[j]);
+		
+	}
+}
